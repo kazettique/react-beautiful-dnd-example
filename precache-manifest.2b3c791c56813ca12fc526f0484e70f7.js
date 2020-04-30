e866635e7d8ed7148f3e72531ee8450dfc196e6c
Woody <woody.chang@cloud-interactive.com>
2020-04-30 22:51:37 +0800

    Updates

---
+++ precache-manifest.2b3c791c56813ca12fc526f0484e70f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd0188a07c73fdf7748021b7cc9ce09",
+    "url": "/react-beautiful-dnd-example/index.html"
+  },
+  {
+    "revision": "4712915ef5cf01194ae4",
+    "url": "/react-beautiful-dnd-example/static/css/main.b16ebcc2.chunk.css"
+  },
+  {
+    "revision": "77f189d0ce4214380742",
+    "url": "/react-beautiful-dnd-example/static/js/2.55278bef.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/react-beautiful-dnd-example/static/js/2.55278bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4712915ef5cf01194ae4",
+    "url": "/react-beautiful-dnd-example/static/js/main.a985045f.chunk.js"
+  },
+  {
+    "revision": "efddc54b52e947ab7e68",
+    "url": "/react-beautiful-dnd-example/static/js/runtime-main.c3e98f52.js"
+  },
+  {
+    "revision": "d1cdbeacbc16975bb8334d1c534dad72",
+    "url": "/react-beautiful-dnd-example/static/media/grip-vertical-solid.d1cdbeac.svg"
+  }
+]);
